9f919f9d63bfa59b0969d7db5369dbe5b3988327
Paul Betts <paul@paulbetts.org>
2016-05-24 14:49:46 -0700

    Add some sanity checks

---
+++ test/recursive-proxy-handler.js
@@ -0,0 +1,30 @@
+import {RecursiveProxyHandler} from '../src/execute-js-func';
+
+describe('RecursiveProxyHandler', function() {
+  it.only('should let me apply a function', function() {
+    var proxy = RecursiveProxyHandler.create('proxy', (names, args) => {
+      console.log(`${JSON.stringify(names)} - ${JSON.stringify(args)}`);
+    });
+
+    let baz = proxy.foo.bar.baz(1,2,3);
+  });
+
+  it('tests ES6 proxies', function() {
+    let createHandler = () => {
+      return {
+        get: function(target, prop) {
+          console.log('Get!');
+          return new Proxy(function() {}, createHandler());
+        },
+
+        apply: function(target, thisArg, argList) {
+          console.log("Apply!");
+          console.log(JSON.stringify(argList));
+        }
+      }
+    };
+
+    var foo = new Proxy({}, createHandler());
+    foo.bar.baz(1,2,3,4);
+  });
+});
