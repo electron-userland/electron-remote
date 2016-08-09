7f85173417107bceb0aeea4d679341584238b9fd
Paul Betts <paul@paulbetts.org>
2016-08-09 16:30:13 -0700

    Add a test

--- test/remote-event.js
+++ test/remote-event.js
@@ -3,15 +3,27 @@ import {fromRemoteWindow} from '../src/remote-event';
 
 const {BrowserWindow} = remote;
 
-describe('fromRemoteWindow', function() {
+describe.only('fromRemoteWindow', function() {
   this.timeout(10*1000);
   
-  it.only('should get the did-finish-load event', async function() {
+  it('should get the ready-to-show event', async function() {
     let bw = new BrowserWindow({width: 500, height: 500, show: false});
     
-    let finished = fromRemoteWindow(bw, 'did-finish-load').take(1).toPromise();
+    let finished = fromRemoteWindow(bw, 'ready-to-show').take(1).toPromise();
     bw.loadURL('https://www.google.com');
     
     await finished;
+    
+    bw.close();
+  });
+  
+  it('should get the dom-ready event', async function() {
+    let bw = new BrowserWindow({width: 500, height: 500, show: false});
+    
+    let finished = fromRemoteWindow(bw, 'dom-ready', true).take(1).toPromise();
+    bw.loadURL('https://www.google.com');
+    
+    await finished;
+    bw.close();
   });
 });
