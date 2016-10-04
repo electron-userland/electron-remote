731578120e26335099609a8258cf315b3dbffd50
Paul Betts <paul@paulbetts.org>
2016-10-04 12:58:45 -0700

    hwhoops

--- src/renderer-require.js
+++ src/renderer-require.js
@@ -14,6 +14,7 @@ import 'rxjs/add/observable/defer';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/mergeAll';
 import 'rxjs/add/operator/mergeMap';
+import 'rxjs/add/operator/multicast';
 import 'rxjs/add/operator/take';
 import 'rxjs/add/operator/toPromise';
 
